37ac49cf6db2cd057987847559d9f3c5214e48f9
Tim Nooteboom <tim.nooteboom@dhigroupinc.com>
2023-01-07 12:35:12 -0600

    refined the code

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,43 @@
 <template>
-  <div class="w-full h-full flex justify-center items-center">
-    <span class="text-3xl">Good luck!</span>
+  <div style="margin-top: 30%; margin-left:25%">
+    <div class="text-3xl">{{joke}}</div>
+    <div v-if="showAnswer">{{ans}}</div>
+    <div>
+      <button v-if="!showAnswer" @click="jokeAnswer">Show Answer</button>
+      <button style="margin-top:25px" @click="getJoke">New Joke</button>
+    </div>
   </div>
 </template>
 
 <script setup>
-// fetch('https://v2.jokeapi.dev/joke/christmas') ...
+import { ref } from 'vue'
+let joke = ref(null)
+let ans = ref(null)
+let showAnswer = ref(false)
+
+const getJoke = async() => {
+  showAnswer.value = false
+  try {
+    const res = await fetch('https://v2.jokeapi.dev/joke/christmas').then(response => response.json())
+    joke.value = res.setup
+    ans.value = res.delivery
+  } catch (error) {
+      console.error(error)
+      alert('Something went wrong! The joke could not load.')
+  }
+}
+
+const jokeAnswer = () => {
+  showAnswer.value = true
+}
+getJoke()
+
 </script>
+
+<style>
+  button {
+    width: 130px;
+    border: 1px solid;
+    display: block;
+  }
+</style>
